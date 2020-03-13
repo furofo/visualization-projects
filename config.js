9858bdd1d344331e3c1a551a9aad7a8f46c91b83
furofo <chriscellphonerepair@gmail.com>
2020-03-12 23:28:50 -0400

    added logic for api call made two variables for years and gdp x and y values respectably

--- config.js
+++ config.js
@@ -1,15 +1,27 @@
-window.onload = function(){
-    this.console.log("hello");
-    var json;
+var json;
+$(document).ready(function() {  
+    
+    
       
         // Add your code below this line
         fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
       .then(response => response.json())
       .then(data => {
-          json = JSON.stringify(data.data);
+         let json = JSON.parse(JSON.stringify(data));
+          console.log(json);
+          let years = [];
+          let gdp = [];
+          for(let i = 0; i < json.data.length; i++) {
+            let tempYear = json.data[i][0].slice(0, 4);
+            let tempGdp = json.data[i][1];
+            years.push(tempYear);
+            gdp.push(tempGdp);
+          }
+          console.log(years[7]);
+          console.log(gdp[7]);
       });
         
         // Add your code above this line
-    console.log(json);
-    
-    };
+   
+});
+
