279810553d71df900a594ec57ee0b5c0fc0a5b61
furofo <chriscellphonerepair@gmail.com>
2020-03-18 16:21:55 -0400

    drew basic scatter plot with no logic adding logic later

--- Scatterplot/config.js
+++ Scatterplot/config.js
@@ -12,10 +12,23 @@ $(document).ready(function() {
           const w = 500;
           const h = 500;
 
-          const svg = d3.select("container")
+          const svg = d3.select(".container")
                         .append("svg")
                         .attr("width", w)
                         .attr("height", h);
+
+          svg.selectAll("circle")
+              .data(json)
+              .enter()
+              .append("circle")
+              .attr("cx", (d, i) => {
+                console.log("this is i * 7" + i * 7);
+                return i * 7;
+              })
+              .attr("cy", (d, i) => {
+                console.log("this is i * 3" + i * 3);
+                return i * 3})
+              .attr("r", 5);
       });
         
         
