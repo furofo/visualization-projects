435cbd57a15b32dd4ca6518cf536ee0efffb9107
furofo <chriscellphonerepair@gmail.com>
2020-03-19 11:51:37 -0400

    added padding to bottom removed comma from year on x axis

--- Scatterplot/config.js
+++ Scatterplot/config.js
@@ -4,29 +4,34 @@ $(document).ready(function() {
         let makeScatterPlot = function (json) {
           const w = 800;
           const h = 500;
-          const padding = {left: 70, top: 20, right: 50, bottom: 100};
-          console.log(" this is max " + d3.max(json, (d) => d.Year));
+          const padding = {left: 70, top: 20, right: 50, bottom: 40};
           const xScale = d3.scaleLinear()
-                           .domain([d3.min(json, (d) => parseInt(d.Year)), d3.max(json, (d) => parseInt(d.Year))])
+                           .domain([d3.min(json, (d) => d.Year), d3.max(json, (d) => d.Year)])
                            .range([padding.left, w - padding.left]);
           
           const yScale = d3.scaleLinear()
                             .domain([d3.min(json, (d) => parseInt(d.Seconds)), d3.max(json, (d) => parseInt(d.Seconds))])
-                            .range([padding.top, h]);
-          const xAxis = d3.axisBottom(xScale);
+                            .range([padding.top, h - padding.bottom]);
+          const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
           const yAxis =d3.axisLeft(yScale);   
           const svg = d3.select(".container")
                         .append("svg")
                         .attr("width", w)
                         .attr("height", h);
           const g = svg.append("g");
+          const date = json.map(x => {
+           return new Date(x.Year, 0);
+          });
+          let date1 = new Date(1997, 0);
+          console.log("this is date 1" + date1);
+          console.log("this is date " + date[1].getFullYear());
 
           g.selectAll("circle")
               .data(json)
               .enter()
               .append("circle")
               .attr("cx", (d, i) => {
-                console.log("this is xScale for year" + xScale(d.Year));
+                console.log("this is Xcale " + (d.Year));
                 return xScale(d.Year);
               })
               .attr("cy", (d, i) => {
@@ -34,11 +39,11 @@ $(document).ready(function() {
               .attr("r", 5);
 
             svg.append("g")
-              .attr("transform", "translate(-5," + (h - 25) + ")") // make x-axis
+              .attr("transform", "translate(-5," + (h - padding.bottom) + ")") // make x-axis
               .attr("id", "x-axis")
               .call(xAxis);   
            svg.append("g")
-             .attr("transform", "translate(40," + 0 + ")") // make y -axis
+             .attr("transform", "translate(55," + 0 + ")") // make y -axis
              .attr("id", "y-axis")
              .call(yAxis);
         }
