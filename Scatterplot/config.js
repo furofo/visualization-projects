10b79b77bf88d121a672eb1a4475d89ac856c7ca
furofo <chriscellphonerepair@gmail.com>
2020-03-18 21:47:33 -0400

    started working on axises working on separate branch until figure it out

--- Scatterplot/config.js
+++ Scatterplot/config.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {  
 
         let makeScatterPlot = function (json) {
-          const w = 500;
+          const w = 800;
           const h = 500;
           const padding = {left: 70, top: 20, right: 50, bottom: 100};
           console.log(" this is max " + d3.max(json, (d) => d.Year));
@@ -13,7 +13,8 @@ $(document).ready(function() {
           const yScale = d3.scaleLinear()
                             .domain([d3.min(json, (d) => parseInt(d.Seconds)), d3.max(json, (d) => parseInt(d.Seconds))])
                             .range([padding.top, h]);
-
+          const xAxis = d3.axisBottom(xScale);
+          const yAxis =d3.axisLeft(yScale);   
           const svg = d3.select(".container")
                         .append("svg")
                         .attr("width", w)
@@ -31,6 +32,15 @@ $(document).ready(function() {
               .attr("cy", (d, i) => {
                 return yScale(d.Seconds) - 80})
               .attr("r", 5);
+
+            svg.append("g")
+              .attr("transform", "translate(-5," + (h - 25) + ")") // make x-axis
+              .attr("id", "x-axis")
+              .call(xAxis);   
+           svg.append("g")
+             .attr("transform", "translate(40," + 0 + ")") // make y -axis
+             .attr("id", "y-axis")
+             .call(yAxis);
         }
 
 
